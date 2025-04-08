693b5ef5f7fc5bc19e0492ae72ab43c515555564
plutoistheplanet <plutoboy12@gmail.com>
2025-04-08 17:09:49 +0100

    Light mode

--- js/preferences.js
+++ js/preferences.js
@@ -1,22 +1,15 @@
 const themes = ["defaultMode", "lightMode"]; // add more themes here
 
 function applyTheme(theme) {
-  // Remove all known themes
   themes.forEach(t => document.body.classList.remove(t));
-  // Apply the selected theme
   document.body.classList.add(theme);
-  // Save it
   localStorage.setItem("theme", theme);
 }
 
 function loadSavedTheme() {
   const savedTheme = localStorage.getItem("theme");
   console.log(savedTheme);
-  if (themes.includes(savedTheme)) {
-    applyTheme(savedTheme);
-  } else {
-    applyTheme("lightMode"); // default theme
-  }
+  applyTheme(savedTheme);
 }
 
 loadSavedTheme();
